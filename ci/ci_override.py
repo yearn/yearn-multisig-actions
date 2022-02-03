00dfb5dea23f546ef46381019fa914370f5af7a7
listonjesse <listonjesse@yahoo.com>
2022-02-02 20:21:14 -0800

    chore: add be and bump ci

--- ci/ci_override.py
+++ ci/ci_override.py
@@ -1,6 +1,6 @@
 import os
-from ape_safe import ApeSafe
-from brownie import accounts
+from ape_safe import ApeSafe, transaction_service
+from brownie import accounts, network
 from gnosis.safe.safe_tx import SafeTx
 from typing import Optional, Union
 from brownie.network.account import LocalAccount
@@ -14,8 +14,36 @@ from brownie.network.account import LocalAccount
 DELEGATE_ADDRESS = os.environ.get("DELEGATE_ADDRESS")
 home_directory = os.environ.get("HOME")
 
+gnosis_frontend_urls = {
+    'gnosis': {
+        1: 'https://gnosis-safe.io/app/eth:{0}/transactions/queue',
+        4: 'https://gnosis-safe.io/app/rin:{0}/transactions/queue',
+        56: 'https://gnosis-safe.io/app/bsc:{0}/transactions/queue',
+        100: 'https://gnosis-safe.io/app/xdai:{0}/transactions/queue',
+        137: 'https://gnosis-safe.io/app/matic:{0}/transactions/queue',
+        250: 'https://safe.fantom.network/#/safes/{0}/transactions',
+        42161: 'https://gnosis-safe.io/app/arbi:{0}/transactions/queue'
+    }
+}
 
 class DelegateSafe(ApeSafe):
+    def __init__(self, address, base_url=None, multisend=None):
+        """
+        Create an ApeSafe from an address or a ENS name and use a default connection.
+        """
+        backend_urls = {
+            'gnosis': transaction_service,
+        }
+
+        # default to gnosis if we don't have a custom version
+        if network.chain.id not in backend_urls[self.backend_type]:
+            backend_url_from_config = backend_urls["gnosis"][network.chain.id]
+        else:
+            backend_url_from_config = backend_urls[self.backend_type][network.chain.id]
+
+        self.base_url = base_url or backend_url_from_config
+        super().__init__(address, base_url, multisend)
+
     @property
     def is_ci(self):
         return os.environ.get("CI", "").lower() == "true"
@@ -23,6 +51,11 @@ class DelegateSafe(ApeSafe):
     @property
     def is_send(self):
         return os.environ.get("GITHUB_ACTION_SEND", "").lower() == "true"
+
+    @property
+    def backend_type(self):
+        backend_from_env = os.environ.get("BE", "").lower()
+        return backend_from_env if backend_from_env != "" else "gnosis"
 
     def post_transaction(self, safe_tx: SafeTx):
         super().post_transaction(safe_tx)
@@ -41,6 +74,9 @@ class DelegateSafe(ApeSafe):
 
     def get_signer(self, signer: Optional[Union[LocalAccount, str]] = None) -> LocalAccount:
         if self.is_ci:
+            with open(os.path.join(home_directory, "safe.txt"), "w") as f:
+                f.write(str(self.frontend_url.format(self.address)))
+
             if self.is_send:
                 key = os.environ.get("PRIVATE_KEY")
                 assert (
